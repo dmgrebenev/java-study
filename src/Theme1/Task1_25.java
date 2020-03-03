3cd62e27b52ef041901adb2bb1efe79649b4c870
Dmitriy Grebenev <d.grebenev@taxi-master.com>
2020-03-03 12:23:34 +0400

    Task 1.25 решено

--- src/Theme1/Task1_25.java
+++ src/Theme1/Task1_25.java
@@ -4,36 +4,38 @@
 
 package Theme1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Task1_25 {
     public static void main(String[] args) throws Exception {
         InputParameters1_25 coefficients = new InputParameters1_25();
         double discriminant = discriminant(coefficients.a(), coefficients.b(), coefficients.c());
-
-        if (discriminant < 0){returnNoDecision();}
-        else if (discriminant == 0){returnDecision(coefficients);}
-        else {returnTwoDecision(coefficients, discriminant);}
+        searchDecision(coefficients, discriminant);
     }
 
-    private static double discriminant(double a, double b, double c) throws Exception{
+    private static double discriminant(double a, double b, double c){
         return b * b - 4 * a * c;
+    }
+
+    private static void searchDecision(InputParameters1_25 coefficients, double discriminant){
+        if (discriminant < 0) { returnNoDecision(); }
+        else { decision(coefficients, discriminant); }
     }
 
     private static void returnNoDecision(){
         System.out.println("Уравнение не имеет решения");
     }
 
-    private static void returnDecision(InputParameters1_25 coefficients) {
-        double x = Math.sqrt(findX1(coefficients, 0));
-        System.out.println("X1 = " + x + "X2 = " + (-x));
-    }
+    private static void decision(InputParameters1_25 coefficients, double discriminant) {
+        Double x1 = findX1(coefficients, discriminant);
+        x1 = Math.sqrt(x1);
+        Double x2 = null;
+        if (discriminant > 0){
+            x2 = findX2(coefficients, discriminant);
+            x2 = Math.sqrt(x2); }
 
-    private static void returnTwoDecision(InputParameters1_25 coefficients, double discriminant) {
-        double x1 = Math.sqrt(findX1(coefficients, discriminant));
-        double x2 = Math.sqrt(findX2(coefficients, discriminant));
-
-        System.out.println("X1 = " + x1 + " " + "X2 = " + " " + (-x1) + " " + "X3 = " + x2 + " " + "X4 = " + (-x2));
+        resultBuilder(x1, x2);
     }
 
     private static double findX1(InputParameters1_25 coefficients, double discriminant){
@@ -42,6 +44,25 @@ public class Task1_25 {
 
     private static double findX2(InputParameters1_25 coefficients, double discriminant){
         return ((-coefficients.b() - Math.sqrt(discriminant)) / ( 2 * coefficients.a()));
+    }
+
+    private static void resultBuilder(Double x1, Double x2){
+        String message = null;
+        DecimalFormat df = new DecimalFormat("###.###");
+        df.format(x1);
+        df.format(x2);
+        if (x1 == 0) {
+            message = "X1 = 0";
+            if (x2 != null && x2 > 0) {message = message + "\nX2 = " + df.format(x2)+ "\nX3 = " + df.format(-x2); }
+        }
+        else if (x1 > 0) {
+            message = "X1 = " + df.format(x1) + "\nX2 = " + df.format(-x1);
+            if (x2 != null && x2 == 0) {message = message + "\nX3 = 0";}
+            else if (x2 != null && x2 > 0) {message = message + "\nX3 = " + df.format(x2)  + "\nX4 = " + df.format(-x2);}
+        }
+
+        if (message != null) {System.out.println(message);}
+        else {returnNoDecision();}
     }
 
 }
