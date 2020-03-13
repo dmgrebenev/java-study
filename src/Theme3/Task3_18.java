ee3566cfd3698f815d5426699edeb6896e2b566d
Dmitriy Grebenev <d.grebenev@taxi-master.com>
2020-03-13 17:05:12 +0400

    3.18 правки

--- src/Theme3/Task3_18.java
+++ src/Theme3/Task3_18.java
@@ -15,11 +15,53 @@ public class Task3_18 {
     public static void main(String[] args) throws Exception {
         InputParameters input = new InputParameters("3_18");
 
+        int countLine = lineCounter(input.file);
+        String[] arrayOfLine = allLineToArray(input.file, countLine);
 
+        String[][] newMatrixOfLine = new String[countLine][2];
+        for (int i = 0; i < arrayOfLine.length; i++) {
+            String[] words = arrayOfLine[i].split(" ");
+            double sum = 0.0;
+            int count = 0;
+            for (int j = 0; j < words.length; j++) {
+                try {
+                    sum += Double.parseDouble(words[j]);
+                    count ++;
+                }
+                catch (NumberFormatException e) {
+                    if (newMatrixOfLine[i][0] == null) {
+                        newMatrixOfLine[i][0] = words[j] + " ";
+                    }
+                    else {
+                        newMatrixOfLine[i][0] += words[j] + " ";
+                    }
+                }
+            }
+            newMatrixOfLine[i][1] = String.valueOf((sum / count));
+        }
 
-        String[] arrayOfLine = new String[10];
+        returnFile(newMatrixOfLine, input.file);
+    }
+
+    //  Подсчет количества строк в файле
+    private static int lineCounter(File file) {
+        int i = 0;
         try {
-            FileReader fr = new FileReader(input.file);
+            FileReader fr = new FileReader(file);
+            Scanner scan = new Scanner(fr);
+            while (scan.hasNextLine()) {
+                i++;
+                scan.nextLine();
+            }
+            scan.close();
+        } catch (FileNotFoundException e) { System.err.println(e); }
+        return i;
+    }
+
+    private static String[] allLineToArray(File file, int countLine) {
+        String[] arrayOfLine = new String[countLine];
+        try {
+            FileReader fr = new FileReader(file);
             Scanner scan = new Scanner(fr);
             int i = 0;
             // чтение из файла
@@ -29,17 +71,25 @@ public class Task3_18 {
             }
             scan.close();
         } catch (FileNotFoundException e) { System.err.println(e); }
+        return arrayOfLine;
+    }
 
-        for (String value : arrayOfLine) {
-            System.out.println(value);
+    private static void returnFile(String[][] matrix, File file) throws IOException {
+        FileWriter fw = null;
+
+        try {
+            fw = new FileWriter(file + ".new", true);
+        } catch (IOException e) {
+            System.err.println("ошибка открытия потока " + e);
+            System.exit(1);
         }
+
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+
+        for (int i = 0; i < matrix.length; i++) {
+            pw.printf(matrix[i][0] + matrix[i][1] + "\n");
+        }
+        pw.close();
     }
-
-//  Подсчет количества строк в файле
-    private static int lineCounter(File file) {
-        int i = 0;
-
-        return i;
-    }
-
 }
